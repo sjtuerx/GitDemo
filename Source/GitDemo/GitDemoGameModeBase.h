53def998cd3fbf217b5ab4e2b61aca337f312044
sjtuerxu <xusjtuer@163.com>
2022-01-31 12:28:53 +0800

    init

---
+++ Source/GitDemo/GitDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GitDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GITDEMO_API AGitDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
